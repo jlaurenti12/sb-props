c1d633a07195d69d16c5cc728f4e9acb8b368c84
jlaurenti12 <jake.laurenti@gmail.com>
2025-02-03 23:00:06 -0500

    set doc on register

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -76,7 +76,7 @@ const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
-    await addDoc(collection(db, "users", user.uid), {
+    await setDoc(doc(db, "users", user.uid), {
       uid: user.uid,
       name,
       authProvider: "local",
